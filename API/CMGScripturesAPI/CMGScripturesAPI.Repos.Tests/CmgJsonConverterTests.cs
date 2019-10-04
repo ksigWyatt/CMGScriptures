834d5dd2e7bc76ad3449c4b48e7c97e92602b039
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-10-04 09:34:22 -0400

    Added XUnit tests for CmgJsonConverter

---
+++ API/CMGScripturesAPI/CMGScripturesAPI.Repos.Tests/CmgJsonConverterTests.cs
@@ -0,0 +1,18 @@
+using CMGScripturesAPI.Repos.System;
+using System;
+using System.IO;
+using Xunit;
+
+namespace CMGScripturesAPI.Repos.Tests {
+    public class CmgJsonConverterTests {
+        [Fact]
+        public void ConvertImageResponseToDto_Valid_ReturnsCorrectNumber() {
+            int expected = 36;
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string sampleJsonPath = $"{projectDirectory}\\Samples\\CmgGetResponseSample.json";
+            string sampleJson = File.ReadAllText(sampleJsonPath);
+            var imageDtos = CmgJsonConverter.ConvertImageResponseToDto(sampleJson);
+            Assert.Equal(expected, imageDtos.Count);
+        }
+    }
+}
