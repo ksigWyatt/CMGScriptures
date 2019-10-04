a3c3c0d70f71caa7266627343b7761b280705d10
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-10-03 22:55:27 -0400

    Added MetaDto

---
+++ API/CMGScripturesAPI/CMGScripturesAPI.Repos/DTOs/MetaDto.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMGScripturesAPI.Repos.DTOs {
+    public class MetaDto {
+        [JsonProperty]
+        public int RequestTotal { get; set; }
+        [JsonProperty]
+        public int RequestStart { get; set; }
+        [JsonProperty]
+        public int RequestEnd { get; set; }
+        [JsonProperty]
+        public int Total { get; set; }
+    }
+}
