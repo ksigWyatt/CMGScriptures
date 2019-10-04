705fb966c732e21679de8971429346929af86c81
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-10-04 09:10:05 -0400

    Added Creator property & commented out unwanted properties

--- API/CMGScripturesAPI/CMGScripturesAPI.Repos/DTOs/CmgImageDto.cs
+++ API/CMGScripturesAPI/CMGScripturesAPI.Repos/DTOs/CmgImageDto.cs
@@ -5,17 +5,18 @@ using System.Text;
 namespace CMGScripturesAPI.Repos.DTOs {
     public class CmgImageDto {
         public string Id { get; set; }
-        public string Permalink { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; }
+        //public string Permalink { get; set; }
+        //public string Name { get; set; }
+        //public string Type { get; set; }
         public DateTime Added { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
+        public CreatorDto Creator { get; set; }
         public string Thumbnail { get; set; }
         public string ThumbnailLg { get; set; }
         public string ThumbnailXLg { get; set; }
-        public int Downloads { get; set; }
-        public bool IsFree { get; set; }
+        //public int Downloads { get; set; }
+        //public bool IsFree { get; set; }
         public string Filename { get; set; }
 
 
