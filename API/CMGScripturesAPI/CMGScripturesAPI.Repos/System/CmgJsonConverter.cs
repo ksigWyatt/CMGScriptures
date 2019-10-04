5151635c94a0c403b28fe55e7fc93eaf90930a41
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-10-04 09:33:49 -0400

    Added CmgJsonConverter

---
+++ API/CMGScripturesAPI/CMGScripturesAPI.Repos/System/CmgJsonConverter.cs
@@ -0,0 +1,15 @@
+﻿using CMGScripturesAPI.Repos.DTOs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CMGScripturesAPI.Repos.System {
+    public static class CmgJsonConverter {
+        public static List<CmgImageDto> ConvertImageResponseToDto(string responseJson) {
+            var result = JsonConvert.DeserializeObject<CmgResponseOuterDto>(responseJson);
+            return result.images.ToList();
+        }
+    }
+}
