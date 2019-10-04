1365e1450be6b91a12229827119da700a38df38d
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-10-04 09:01:37 -0400

    Added Dtos for deserializing CMG JSON

---
+++ API/CMGScripturesAPI/CMGScripturesAPI.Repos/DTOs/CmgImageDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CMGScripturesAPI.Repos.DTOs {
+    public class CmgImageDto {
+        public string Id { get; set; }
+        public string Permalink { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime Added { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Thumbnail { get; set; }
+        public string ThumbnailLg { get; set; }
+        public string ThumbnailXLg { get; set; }
+        public int Downloads { get; set; }
+        public bool IsFree { get; set; }
+        public string Filename { get; set; }
+
+
+
+    }
+}
