3e8ab9f5258f063b4d40891cce4d513d8641d64c
Lucas Marques <lucas.msilva09@gmail.com>
2019-10-12 11:53:59 -0300

    Creating the base structure to Home and Editor component

--- UI/CMGScriptures/src/app/app-routing.module.ts
+++ UI/CMGScriptures/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EditorComponent } from './editor/editor.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {path: "", component: HomeComponent},
+  {path: "editor", component: EditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UI/CMGScriptures/src/app/app.component.ts
+++ UI/CMGScriptures/src/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'CMGScriptures';
+export class AppComponent {  
 }
